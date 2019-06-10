15471badda03cdc733ca54060243989d17178fb5
Joory <jooryde@yeah.net>
2019-06-10 17:37:42 +0800

    204 primes number (did not finish)

---
+++ 204.count-primes.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=204 lang=cpp
+ *
+ * [204] Count Primes
+ */
+class Solution
+{
+public:
+    int countPrimes(int n)
+    {
+        if (n <= 1)
+            return 0;
+        if (n == 2)
+            return 1;
+        int res = 0;
+        for (int i = 2; i <= n; i++)
+        {
+        }
+    }
+};
