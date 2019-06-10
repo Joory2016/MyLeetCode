264ff00294730263991377268436c4e8608d75ce
Jooryde <jooryde@yeah.net>
2019-06-10 23:30:46 +0800

    204. slow method to count primes

--- 204.count-primes.cpp
+++ 204.count-primes.cpp
@@ -8,13 +8,28 @@ class Solution
 public:
     int countPrimes(int n)
     {
-        if (n <= 1)
-            return 0;
-        if (n == 2)
-            return 1;
         int res = 0;
-        for (int i = 2; i <= n; i++)
+        for (int i = 1; i < n; i++)
         {
+            if (isPrime(i))
+                res++;
         }
+        return res;
+    }
+
+private:
+    bool isPrime(int n)
+    {
+        if (n < 2)
+            return false;
+        if (n == 2)
+            return true;
+        int temp = sqrt(n);
+        for (int i = 2; i <= temp; i++)
+        {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
     }
 };
