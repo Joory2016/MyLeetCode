5338968dfbaf662075fda48d201a2a7254114439
Joory <jooryde@yeah.net>
2019-06-11 17:37:19 +0800

    204. fast method, but did not give right answer

--- 204.count-primes.cpp
+++ 204.count-primes.cpp
@@ -35,7 +35,7 @@ private:
     }
     
 
-};*/
+};
 
 class Solution
 {
@@ -75,4 +75,34 @@ private:
     }
 
     vector<int> primes;
+};
+*/
+
+class Solution
+{
+public:
+    int countPrimes(int n)
+    {
+        if (n < 2)
+            return 0;
+        bool *isPrime = new bool(n);
+        for (int i = 2; i < n; i++)
+            isPrime[i] = true;
+
+        for (int i = 2; i * i < n; i++)
+        {
+            if (!isPrime[i])
+                continue;
+            for (int j = i * i; j < n; j += i)
+                isPrime[j] = false;
+        }
+        int count = 0;
+        for (int i = 2; i < n; i++)
+        {
+            if (isPrime[i])
+                count++;
+        }
+        delete[] isPrime;
+        return count;
+    }
 };
