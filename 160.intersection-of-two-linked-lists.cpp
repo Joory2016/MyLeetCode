c956389c2c20b9c7008fb17cbaad376443a2fb83
Jooryde <jooryde@yeah.net>
2019-06-12 23:18:33 +0800

    160. intersection node --using two pointers

--- 160.intersection-of-two-linked-lists.cpp
+++ 160.intersection-of-two-linked-lists.cpp
@@ -11,6 +11,7 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+/*
 class Solution
 {
 public:
@@ -29,5 +30,53 @@ public:
             headB = headB->next;
         }
         return nullptr;
+    }
+};
+ */
+
+class Solution
+{
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+    {
+        int lenA = countLen(headA);
+        int lenB = countLen(headB);
+        ListNode *longList = nullptr;
+        ListNode *shortList = nullptr;
+        int diff = 0;
+        if (lenA >= lenB)
+        {
+            diff = lenA - lenB;
+            longList = headA;
+            shortList = headB;
+        }
+        else
+        {
+            diff = lenB - lenA;
+            longList = headB;
+            shortList = headA;
+        }
+        for (int i = 0; i < diff; i++)
+            longList = longList->next;
+        while (longList != shortList)
+        {
+            if (longList == nullptr)
+                return nullptr;
+            longList = longList->next;
+            shortList = shortList->next;
+        }
+        return longList;
+    }
+
+private:
+    int countLen(ListNode *head)
+    {
+        int len = 0;
+        while (head != nullptr)
+        {
+            len++;
+            head = head->next;
+        }
+        return len;
     }
 };
